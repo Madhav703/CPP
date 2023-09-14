9773293d4a4b49b8766c5666c6750b8984cc8e11
madhav_0703 <122460940+Madhav703@users.noreply.github.com>
2023-09-14 18:17:54 +0530

    Create _main.cpp

---
+++ _main.cpp
@@ -0,0 +1,18 @@
+// Star Pattern
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  int i, j, row=6;   
+    
+  for(i=0; i<row; i++){   
+    for(j=0; j<=i; j++){   
+      cout << "* ";   
+    }   
+    cout << endl; 
+  }
+  
+  return 0;
+}
