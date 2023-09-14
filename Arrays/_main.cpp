3d613eea939d7bebcb0cc4097632dd28be2c447e
madhav_0703 <122460940+Madhav703@users.noreply.github.com>
2023-09-14 18:45:47 +0530

    Create _main.cpp

---
+++ Arrays/_main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+  
+  string names[] = {"Car","Bike","Plane","Jet"};
+    
+		for(int i=0;i<4;i++){
+	   cout << names[i] << endl;
+	 }
+
+  return 0;
+}
